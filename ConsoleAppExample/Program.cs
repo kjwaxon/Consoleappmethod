b8a1be36468f67cb36b184305530fbb31efa7521
kjwaxon <kaanaslan1996@hotmail.com>
2022-04-19 14:39:00 +0300

    Consoleappmethod

---
+++ ConsoleAppExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleAppExample
+{
+
+    class bilgisayar
+    {
+        public string ekrankartı;
+        public string işlemci;
+        public int ram;
+
+        public void yazdır()
+        {
+            Console.WriteLine("Bilgisayarınızın ekran kartı : " + ekrankartı);
+            Console.WriteLine("Bilgisayarınızın işlemcisi : " + işlemci);
+            Console.WriteLine("Bilgisayarınızın ram sayısı :{0}gb " , ram );
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bilgisayar pc = new bilgisayar();
+            pc.ekrankartı = "Rtx 3090";
+            pc.işlemci = "i7 9900k";
+            pc.ram = 32;
+
+            pc.yazdır();
+
+        }
+    }
+}
